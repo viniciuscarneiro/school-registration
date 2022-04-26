b92a928a38a78419e38dfe1e717f11dbae8db75c
Vinícius Carneiro de Brito <viniciuscbrito89@gmail.com>
2022-04-26 18:43:47 -0300

    new tests

--- src/main/java/io/metadata/schoolregistration/domain/usecase/course/read/FindByIdUseCase.java
+++
@@ -1,7 +0,0 @@
-package io.metadata.schoolregistration.domain.usecase.course.read;
-
-import io.metadata.schoolregistration.domain.entity.Course;
-
-public interface FindByIdUseCase {
-    Course execute(Long courseId);
-}
--- src/test/java/io/metadata/schoolregistration/domain/usecase/student/FetchAllSummarizedUseCaseTest.java
+++ src/test/java/io/metadata/schoolregistration/domain/usecase/student/FetchAllSummarizedUseCaseTest.java
@@ -1,7 +1,7 @@
 package io.metadata.schoolregistration.domain.usecase.student;
 
 import io.metadata.schoolregistration.domain.gateway.StudentGateway;
-import io.metadata.schoolregistration.domain.usecase.student.read.FindAllDetailedUseCaseImpl;
+import io.metadata.schoolregistration.domain.usecase.student.read.FetchAllSummarizedUseCaseImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,18 +14,18 @@ import java.util.List;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class FindAllDetailedUseCaseTest {
+class FindAllSummarizedUseCaseTest {
 
     @Mock
     private StudentGateway studentGateway;
 
     @InjectMocks
-    private FindAllDetailedUseCaseImpl findAllDetailedUseCase;
+    private FetchAllSummarizedUseCaseImpl findAllSummarizedUseCase;
 
     @Test
     void should_find_all() {
-        when(studentGateway.findAll(Boolean.TRUE)).thenReturn(List.of());
-        Assertions.assertDoesNotThrow(() -> findAllDetailedUseCase.execute());
-        verify(studentGateway, times(1)).findAll(Boolean.TRUE);
+        when(studentGateway.findAll(Boolean.FALSE)).thenReturn(List.of());
+        Assertions.assertDoesNotThrow(() -> findAllSummarizedUseCase.execute());
+        verify(studentGateway, times(1)).findAll(Boolean.FALSE);
     }
 }
